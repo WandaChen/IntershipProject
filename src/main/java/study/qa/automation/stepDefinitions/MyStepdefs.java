6ff72a8f7be062a88f9507c0b1c2c199f5442d3b
W_Chen <wanda.chen@gmail.com>
2018-09-07 17:03:19 -0700

    [ADDED]<Automate TC for Quiz Property Title ASK-69/Bug-ASK-482/479/480

--- src/main/java/study/qa/automation/stepDefinitions/MyStepdefs.java
+++ src/main/java/study/qa/automation/stepDefinitions/MyStepdefs.java
@@ -8,6 +8,7 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.But;
 import org.apache.commons.io.FileUtils;
+import org.assertj.core.api.AbstractBooleanAssert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,11 +17,14 @@ import java.util.Date;
 import java.util.Iterator;
 import static org.assertj.core.api.Assertions.*;
 import static study.qa.automation.utils.TestContext.getDriver;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.Alert;
 
 public class MyStepdefs {
 
     @Then ("^I click on element with xpath \"([^\"]*)\"$")
     public void iClickOnElementWithXpath(String xpath) {
+
         getDriver().findElement(By.xpath(xpath)).click();
     }
 
@@ -40,4 +44,38 @@ public class MyStepdefs {
         getDriver().findElement(By.xpath(xpath)).sendKeys(text);
     }
 
-}
+    @Then("^I should see element with xpath \"([^\"]*)\" should be masked$")
+    public void iShouldSeeElementWithXpathShouldBeMasked(String xpath) throws
+            NoAlertPresentException,InterruptedException  {
+        WebElement input = getDriver().findElement(By.xpath(xpath));
+        boolean isEncrypted = input.getAttribute("type").equals("password");
+        if (isEncrypted)
+            System.out.println("The password is masked!");
+        else {
+            System.out.println("Warning! The password is not masked!");
+            // Switching to Alert
+            Alert alert = getDriver().switchTo().alert();
+            // Capturing alert message.
+            String alertMessage= getDriver().switchTo().alert().getText();
+            // Displaying alert message
+            System.out.println(alertMessage);
+            Thread.sleep(5000);
+            // Accepting alert
+            alert.accept();
+        }
+    }
+
+    @Then("^I copy \"([^\"]*)\" into element with xpath \"([^\"]*)\"$")
+    public void iCopyIntoElementWithXpath(String text, String xpath) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement key1 =  getDriver().findElement(By.xpath(xpath));
+        key1.sendKeys(Keys.CONTROL, "c");
+        assertThat(key1.isEnabled()).isFalse();
+    }
+
+    @Then("^element with xpath \"([^\"]*)\" should be presented$")
+    public void elementWithXpathShouldBePresented(String xpath) {
+        assertThat(getDriver().findElements(By.xpath(xpath))).hasSize(1);
+    }
+
+ }
